9881c96093ee157629b4cea8cffecc687a2f14dc
kjahn <kjahn@chosun.ac.kr>
2018-08-29 01:32:34 +0900

    working

---
+++ Get_patches_3D_zend.m
@@ -0,0 +1,42 @@
+V_cb_1_azend = V_cb_1 * (azbegin/azend);
+V_cb_2_azend = V_cb_2 * (azbegin/azend);
+V_cb_3_azend = V_cb_3 * (azbegin/azend);
+
+a           = azend;
+Get_D_dDda;  %%==== script: should always be preceeded by scale factor a.
+Dc3D_azend  = Deltagro*Dpg + Deltadec*Dpd +fb*(Deltacom + Deltastr*Dms);
+Db3D_azend  = Deltagro*Dpg + Deltadec*Dpd -fc*(Deltacom + Deltastr*Dms);
+THc3D_azend = -aH*(Deltagro*dDpg_da + Deltadec*dDpd_da) -aH*fb*Deltastr*dDms_da;
+THb3D_azend = -aH*(Deltagro*dDpg_da + Deltadec*dDpd_da) +aH*fc*Deltastr*dDms_da;
+
+%% For DT, first get DTA(at a=0.01) and DTB(at a=0.1), and use
+%% the empirical fit for 4Mpc cell to obtain DeltaT at azend. 
+%% This also prepares for getting DeltaT at any a.
+Get_DeltaT_fit; %%==== script ==================
+
+if matlabflag
+  save([outputdir '/V_cb_1_azend.dat'], 'V_cb_1_azend', '-v6'); % @ azend
+  save([outputdir '/V_cb_2_azend.dat'], 'V_cb_2_azend', '-v6'); % @ azend
+  save([outputdir '/V_cb_3_azend.dat'], 'V_cb_3_azend', '-v6'); % @ azend
+  save([outputdir '/Dc3D_azend.dat'],   'Dc3D_azend',   '-v6'); % @ azend
+  save([outputdir '/Db3D_azend.dat'],   'Db3D_azend',   '-v6'); % @ azend
+  save([outputdir '/THc3D_azend.dat'],  'THc3D_azend',  '-v6'); % @ azend
+  save([outputdir '/THb3D_azend.dat'],  'THb3D_azend',  '-v6'); % @ azend
+  save([outputdir '/DT_azend.dat'],     'DT3D_azend',   '-v6'); % @ azend
+else
+  save('-mat-binary', [outputdir '/V_cb_1_azend.dat'], 'V_cb_1_azend'); % @ azend
+  save('-mat-binary', [outputdir '/V_cb_2_azend.dat'], 'V_cb_2_azend'); % @ azend
+  save('-mat-binary', [outputdir '/V_cb_3_azend.dat'], 'V_cb_3_azend'); % @ azend
+  save('-mat-binary', [outputdir '/Dc3D_azend.dat'],   'Dc3D_azend'  ); % @ azend
+  save('-mat-binary', [outputdir '/Db3D_azend.dat'],   'Db3D_azend'  ); % @ azend
+  save('-mat-binary', [outputdir '/THc3D_azend.dat'],  'THc3D_azend' ); % @ azend
+  save('-mat-binary', [outputdir '/THb3D_azend.dat'],  'THb3D_azend' ); % @ azend
+  save('-mat-binary', [outputdir '/DT_azend.dat'],     'DT3D_azend'  ); % @ azend
+end
+
+if plotflag
+  ifig = ifig+1;
+  figure(ifig);
+  hist2d([Dc3D_azend(:),THc3D_azend(:)],100,100);
+  view(2);
+end
