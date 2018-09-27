e76f6be03abb12b2f399d1c4000c10336b8fdd38
kjyaya <kjahn@chosun.ac.kr>
2018-09-27 21:26:44 +0900

    working

--- src/Set_gaussrand.m
+++ src/Set_gaussrand.m
@@ -27,9 +27,29 @@ if oldseedflag  %% use preexisting seed
     else
       load('-mat-binary', fileNseed, 'randamp', 'randphs')
     end
+  elseif ~exist(fileNseed)  %% use preexisting seed, and also generate & attach higher-k seeds.
+    if matlabflag
+      load(file512seed, '-mat', 'randamp', 'randphs')
+    else
+      load('-mat-binary', file512seed, 'randamp', 'randphs')
+    end
     Nmissing = Nmode_p*Nmode_p*Nc_p - 512*512*257;
+    randamp_  = zeros(Nmode_p, Nmode_p, Nc_p);
+    randphs_  = zeros(Nmode_p, Nmode_p, Nc_p);
     randamp__ = raylrnd(1,      [Nmissing,1]);
     randphs__ = unifrnd(0.2*pi, [Nmissing,1]);
+    istart = (Nmode_p-512)/2 + 1;
+    iend   = istart    + 512 - 1;
+    izstart = Nc_p-257+1;
+
+    Ntemp     = Nmode_p + Nmode_p + Nc_p-257;
+    randamp_(1:Nmode_p, 1:Nmode_p, 1:Nc_p-257) = reshape(randamp__(1:Ntemp), Nmode_p, Nmode_p, Nc_p-257);
+    Ntemp_old = Ntemp;
+    Ntemp     = istart-1 + Nmode_p + 257;
+    randamp_(1:istart-1, 1:Nmode_p, izstart:Nc_p) = reshape(randamp__(Ntemp_old+1:Ntemp_old+Ntemp), istart-1, Nmode_p, 257);
+    randamp_(iend+1:Nmode_p, 1:Nmode_p, izstart:Nc_p) = reshape(randamp__(1:Ntemp), istart-1, Nmode_p, 257);
+    iend+1:Nmode_p
+    1:257-Nc_p
   end
 else
     
