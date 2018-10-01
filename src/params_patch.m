612ca830188bdfe0c9ee9b88926354c57e4a494d
kjahn <kjahn@chosun.ac.kr>
2018-10-02 00:09:58 +0900

    working

--- src/params_patch.m
+++
@@ -1,7 +0,0 @@
-%% Initial condition configuration inside a chosen patch
-Lbox_p     = 1/h       %% box size in Mpc unit; Should be <= Lbox/Ncell
-Ncell_p    = 512       %% # of cells & particles along one axis: make it an even number
-
-plotflag_p = false     %% true if plots of initial conditions wanted
-
-ICdir      = '../ICs';  %% (mother) directory for initial condition output
