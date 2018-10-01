9427afac14330f1665f5945fc7a2e01414925ccb
kjyaya <kjahn@chosun.ac.kr>
2018-10-01 18:38:01 +0900

    working

--- parameters/params_patch.m
+++ parameters/params_patch.m
@@ -4,8 +4,8 @@ Ncell_p   = 508;         %% # of cells & particles along one axis: make it an ev
 
 ICdir     = '../ICs';  %% directory to place initial condition outputs
 
-%% Nseed   denotes [setupdir '/subgaussseed' num2str(Nmode_p) '.matbin'].
-%% 512seed denotes [setupdir '/subgaussseed512.matbin'].
+%% Nseed    denotes [setupdir '/subgaussseed' num2str(Nmode_p)  '.matbin'].
+%% Noldseed denotes [setupdir '/subgaussseed' num2str(Noldseed) '.matbin'].
 oldseedflag    = true;   %% if true, use old seed
 %% Following two parameters needed only when oldseedflag = true
 diroldseed     = setupdir %% directory where old seed exists
@@ -13,8 +13,6 @@ Noldseed       = 512     %% seed with Noldseed*Noldseed*(Noldseed/2+1) elements
 %% name of old seed file = ['subgaussseed' num2str(Noldseed) '.matbin']
 
 recordseedflag = true;   %% if true, record newly generated seed
-%% record512companionflag will be automatically set to false when oldseedflag = true.
-%% The following, if true, will overwrite existing 512seed.
-record512companionflag = false; %% if true, generate companion 512seed and record it. 
+
 
 
